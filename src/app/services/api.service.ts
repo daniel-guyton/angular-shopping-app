6fbfa5d0529daff901cffb007e115ac571a79b63
daniel-guyton <daniel.j.guyton@gmail.com>
2022-09-09 11:53:53 +1200

    Update enviroment variables

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,18 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
+const baseURL = process.env['NG_APP_APP_GW']
 
 @Injectable({
   providedIn: 'root'
 })
+
 
 export class ApiService {
   constructor(
     private http: HttpClient
   ) {}
 
+
+
   getProducts() {
-   return this.http.get<any>(`${process.env['NG_APP_API_GW']}`)
+   return this.http.get<any>(baseURL)
     .pipe(map((res: Response) => {
       console.log(res)
       return res;

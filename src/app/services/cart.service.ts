1c851c072d602b45a2c4151f8d2453a348f1f337
daniel-guyton <daniel.j.guyton@gmail.com>
2022-09-16 10:41:24 +1200

    Update httpOptions

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -20,10 +20,13 @@ export class CartService {
 
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control)-Allow-Methods': 'GET,PUT,POST,DELETE'
     })
   }
-
   addToCart(product: any) {
 
 

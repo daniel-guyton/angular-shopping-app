67da7dd4c1a3120884b8aca0aea035fb32f19226
daniel-guyton <daniel.j.guyton@gmail.com>
2022-09-16 10:46:05 +1200

    Fix typo on HttpHeaders

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -24,7 +24,7 @@ export class CartService {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': 'true',
         'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control)-Allow-Methods': 'GET,PUT,POST,DELETE'
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
     })
   }
   addToCart(product: any) {

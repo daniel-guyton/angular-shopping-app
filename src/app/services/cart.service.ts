7923cb5af6ac995812676f1a6cd28b660261f28b
daniel-guyton <daniel.j.guyton@gmail.com>
2022-09-16 11:56:53 +1200

    Fix import

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { RequestOptions } from 'https';
 import { BehaviorSubject, map } from 'rxjs';
 
 @Injectable({

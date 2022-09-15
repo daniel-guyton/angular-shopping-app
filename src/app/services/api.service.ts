de0eb7dec372f2a570a03cf8c018ed9e4be5078d
daniel-guyton <daniel.j.guyton@gmail.com>
2022-09-16 10:43:48 +1200

    Replace hardcode api url with enviromemt variable

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -29,7 +29,7 @@ export class ApiService {
 
 
   getProducts() {
-   return this.http.get<any>("https://54f87cx2fj.execute-api.ap-southeast-2.amazonaws.com/test")
+   return this.http.get<any>(process.env.NG_APP_API_GW)
     .pipe(map((res: Response) => {
       console.log(res)
       return res;

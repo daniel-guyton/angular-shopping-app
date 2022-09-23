14115f53845a17248ce2f4ecdffb237ada21dac5
daniel-guyton <daniel.j.guyton@gmail.com>
2022-09-23 15:16:35 +1200

    Update cart service to use arrow functions

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,39 +1,34 @@
-import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import { map, Observable } from 'rxjs'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
 
-import { ProductItemWithQty, ProductItem } from 'src/types/types'
+import { ProductItemWithQty, ProductItem } from 'src/types/types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  cartItemList = []
+  cartItemList = [];
 
-  constructor (
-    private readonly http: HttpClient
-  ) {}
+  constructor(private readonly http: HttpClient) {}
 
-  deleteProductFromCart (productObj: ProductItemWithQty): Observable<ProductItemWithQty> {
-    return this.http.delete<ProductItemWithQty>(`${process.env.NG_APP_API_GW}/${productObj.product.id}`)
-  }
+  deleteFromCart = (productObj: ProductItemWithQty): Observable<ProductItemWithQty> =>
+    this.http.delete<ProductItemWithQty>(`${process.env.NG_APP_API_GW}/${productObj.product.id}`);
 
-  addToCart (product: ProductItem): Observable<ProductItem> {
-    return this.http.post<ProductItem>(process.env.NG_APP_API_GW, product)
-  }
+  addToCart = (product: ProductItem): Observable<ProductItem> =>
+    this.http.post<ProductItem>(process.env.NG_APP_API_GW, product);
 
-  getUserCart (): Observable<any> {
-    return this.http.get<never>(process.env.NG_APP_API_GW + '/getUserCart')
-      .pipe(map((res: any) => {
-        this.cartItemList = res
-        return res
-      }))
-  }
+  getUserCart = (): Observable<any> =>
+    this.http.get<never>(process.env.NG_APP_API_GW + '/getUserCart').pipe(
+      map((res: any) => {
+        this.cartItemList = res;
+        return res;
+      })
+    );
 
-  updateCartQuantity (quantity: number, product: number): Observable<number> {
-    return this.http.patch<never>(process.env.NG_APP_API_GW, {
+  updateCartQuantity = (quantity: number, product: number): Observable<any> =>
+    this.http.patch<never>(process.env.NG_APP_API_GW, {
       qty: quantity,
       product
-    })
-  }
+    });
 }

6bb535726e79f139d36af5fcc68e9df76b35df04
daniel-guyton <daniel.j.guyton@gmail.com>
2022-10-03 10:40:02 +1300

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
     userPoolId: 'ap-southeast-2_rMmsZN3Xl',
     userPoolWebClientId: '2gpbt2u3oefa19k9pmpjt5po7g'
   },
-  production: true
+  production: true,
+  api_gw: process.env.NG_APP_API_GW
 };

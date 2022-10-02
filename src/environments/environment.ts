c9f9f75029d7a6aac43ce131e5c500c84ee2f018
daniel-guyton <daniel.j.guyton@gmail.com>
2022-10-03 09:29:12 +1300

    Update enviroment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     userPoolId: 'ap-southeast-2_rMmsZN3Xl',
     userPoolWebClientId: '2gpbt2u3oefa19k9pmpjt5po7g'
   },
-  product: false
+  production: false
 };
 
 /*

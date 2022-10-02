7b7bdd5be09af9c6ee8b1bbf6707fc4c6dd4dcff
daniel-guyton <daniel.j.guyton@gmail.com>
2022-10-03 09:48:48 +1300

    Update

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -14,16 +14,15 @@ export class CartService {
   constructor(private readonly http: HttpClient) {}
 
   deleteProductFromCart = (productObj: ProductItemWithQty): Observable<ProductItemWithQty> =>
-    this.http.delete<ProductItemWithQty>(`${process.env['NG_APP_API_GW']}/${productObj.product.id}`);
+    this.http.delete<ProductItemWithQty>(`${process.env.NG_APP_API_GW}/${productObj.product.id}`);
 
   addToCart = (product: ProductItem): Observable<ProductItem> =>
-    this.http.post<ProductItem>(process.env['NG_APP_API_GW'], product);
+    this.http.post<ProductItem>(process.env.NG_APP_API_GW, product);
 
-  getUserCart = (): Observable<any> =>
-    this.http.get<ProductItemWithQty[]>(process.env['NG_APP_API_GW'] + '/getUserCart');
+  getUserCart = (): Observable<any> => this.http.get<ProductItemWithQty[]>(process.env.NG_APP_API_GW + '/getUserCart');
 
   updateCartQuantity = (quantity: number, product: number): Observable<any> =>
-    this.http.patch<never>(process.env['NG_APP_API_GW'], {
+    this.http.patch<never>(process.env.NG_APP_API_GW, {
       qty: quantity,
       product
     });
